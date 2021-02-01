1bc65a6df90ff3e1da7666d8044a42c51ebc6e29
kkiyama117 <k.kiyama117@gmail.com>
2021-02-01 16:56:39 +0900

    Keep

--- src/react-web-hid.tsx
+++ src/react-web-hid.tsx
@@ -1,22 +1,54 @@
 import * as React from "react";
-import {HidDeviceOptions,HidDeviceFilter, HidStateMachine} from "./logic/Command";
+import {HidDeviceOptions, HidDeviceFilter, HidStateMachine} from "./logic/Command";
 import {useState} from "react";
 
-(function () {
-  if (typeof window === 'undefined') {
-    return;
+interface ChromeNavigator extends Navigator{
+  hid:{
+    chrome.hid
+  }
+}
+export const WebHid = (props: WebhidProps) => {
+
+  (function () {
+    if (typeof window === 'undefined') {
+      return;
+    }
+
+    // Older browsers might not implement HID at all, so we set an empty object first
+    // Some browsers just don't implement it - return a rejected promise with an error
+    // to keep a consistent interface
+    if (!("hid" in navigator)) {
+      (navigator as any).hid = {};
+      return Promise.reject(
+        new Error("HID is not implemented in this browser")
+      );
+    }
+  })();
+
+  const createHandleOpen = (options: HidDeviceFilter): React.EventHandler<React.MouseEvent<HTMLElement>> => async (e) => {
+    // "open"ボタンをクリックしたときの処理
+    try {
+      const stateMachine: HidStateMachine = navigator.hid.getUserSelectedDevices({filter: [options]})
+      await stateMachine.open();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  // Older browsers might not implement HID at all, so we set an empty object first
-  // Some browsers just don't implement it - return a rejected promise with an error
-  // to keep a consistent interface
-  if (!("hid" in navigator)) {
-    (navigator as any).hid = {};
-    return Promise.reject(
-      new Error("HID is not implemented in this browser")
-    );
-  }
-})();
+  return (
+    <div>
+      {/*HID OPEN*/}
+      <div onClick={createHandleOpen(props.deviceOptions)}/>
+      {/*HID CLOSE*/}
+      <div onClick={handleClose}/>
+
+      <div onClick={(e) => console.log("switch on (WIP)")}/>
+      <div onClick={(e) => console.log("switch off (WIP)")}/>
+      <div onClick={(e) => console.log("read data (WIP)")}/>
+    </div>);
+}
+
+
 
 export type WebhidProps = Omit<React.HTMLProps<HTMLElement>, "ref"> & {
   deviceOptions: HidDeviceFilter;
@@ -36,28 +68,8 @@ async function hidDisconnect(e) {
     console.log("hid disconnect");
   }
 }
-export const WebHid = (props:WebhidProps)=>{
-  return (
-    <div>
-      {/*HID OPEN*/}
-      <div onClick={createHandleOpen(props.deviceOptions)}/>
-      {/*HID CLOSE*/}
-      <div onClick={handleClose}/>
 
-      <div onClick={(e) => console.log("switch on (WIP)")}/>
-      <div onClick={e => console.log("switch off (WIP)")}/>
-      <div onClick={e => console.log("read data (WIP)")}/>
-    </div>);
-}
 
-const createHandleOpen=(options:HidDeviceFilter):React.EventHandler<any> => async (e) => {
-  // "open"ボタンをクリックしたときの処理
-  try {
-    const stateMachine:HidStateMachine= e.navigator.hid.getUserSelectedDevices({filter:[options]})
-    await stateMachine.open();
-  } catch (error) {
-    console.log(error);
-  }
-}
+
 
 export default WebHid;
